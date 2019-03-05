fa61b35d6d8d4ee5dfb412c01bb8127cbe4d2377
Yu <bakararedboy@gmail.com>
2019-03-05 13:24:25 +0900

    ユーザーページを追加

--- resources/views/post_detail.blade.php
+++ resources/views/post_detail.blade.php
@@ -6,7 +6,7 @@
     <div class="p-post">
       <div class="p-post__header">
         <div class="p-post__date">{{ $post->created_at->format('Y年 m月d日') }}</div>
-        <div class="p-post__author">{{ $post->user->userName() }}</div>
+        <a href="{{ route('user.profile', [$post->user_id]) }}" class="p-post__author">{{ $post->user->userName() }}</a>
         @if($post->user->id === Auth::id())
         <div class="p-post__edit-btns">
           <a href="{{ url('user/post-edit', [$post->id]) }}" class="c-edit-btn c-edit-btn__edit">編集</a>
