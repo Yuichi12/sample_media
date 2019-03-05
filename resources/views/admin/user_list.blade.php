c16106275472c22be35c28f6a5f0b5ba930438dd
Yu <bakararedboy@gmail.com>
2019-03-06 00:07:11 +0900

    管理者用ページユーザーリストレイアウト修正

--- resources/views/admin/user_list.blade.php
+++ resources/views/admin/user_list.blade.php
@@ -9,9 +9,9 @@
 <div>{{ $user->name }}</div>
 <div>{{ $user->email }}</div>
 @if($user->role === 0)
-<a href="{{ route('admin.ban', [$user->id]) }}" class="js-btn-del" data-message="ユーザーID:{{ $user->id }}のアクセスを禁止しますか？">アクセスを禁止する</a>
+<a href="{{ route('admin.ban', [$user->id]) }}" class="js-btn-del p-admin-unlock" data-message="ユーザーID:{{ $user->id }}のアクセスを制限しますか？">アクセス可</a>
 @else
-<a href="{{ route('admin.unlock', [$user->id]) }}" class="js-btn-del" data-message="ユーザーID:{{ $user->id }}のアクセスを許可しますか？">アクセスを許可する</a>
+<a href="{{ route('admin.unlock', [$user->id]) }}" class="js-btn-del p-admin-ban" data-message="ユーザーID:{{ $user->id }}のアクセスを許可しますか？">アクセス制限中</a>
 @endif
 <br>
 <br>
